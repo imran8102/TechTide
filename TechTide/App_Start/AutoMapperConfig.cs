d1dbb20f1dab06c5e0859caad63a83c223b04f1e
Imran <emranbagheri81@gmail.com>
2024-03-01 14:36:15 +0330

    feat : Preparing the MVC project for designing and developing and adding the required packages

---
+++ TechTide/App_Start/AutoMapperConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+
+namespace CMSArticle.App_Start
+{
+    public class AutoMapperConfig
+    {
+        public static IMapper mapper;
+
+        public static void Configuration()
+        {
+        }
+    }
+}
